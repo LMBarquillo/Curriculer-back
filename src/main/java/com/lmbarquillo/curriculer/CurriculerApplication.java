e2a004b22a936ae64c13a7bfb76d78d29a11e910
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-04-02 19:29:24 +0200

    primer commit

---
+++ src/main/java/com/lmbarquillo/curriculer/CurriculerApplication.java
@@ -0,0 +1,13 @@
+package com.lmbarquillo.curriculer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculerApplication.class, args);
+	}
+
+}
