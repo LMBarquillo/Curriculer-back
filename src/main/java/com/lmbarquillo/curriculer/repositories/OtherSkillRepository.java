5bf4a98436c721bfa4949220bd1e9e433e1f3501
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-04-29 20:34:26 +0200

    Creado endPoint para obtener la lista de las habilidades adicionales

--- src/main/java/com/lmbarquillo/curriculer/repositories/OtherSkillRepository.java
+++ src/main/java/com/lmbarquillo/curriculer/repositories/OtherSkillRepository.java
@@ -1,9 +1,14 @@
 package com.lmbarquillo.curriculer.repositories;
 
 import com.lmbarquillo.curriculer.entities.OtherSkill;
+import com.lmbarquillo.curriculer.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OtherSkillRepository extends JpaRepository<OtherSkill, Long> {
+
+    List<OtherSkill> findAllByUser(User user);
 }
--- src/main/java/com/lmbarquillo/curriculer/services/SkillService.java
+++ src/main/java/com/lmbarquillo/curriculer/services/SkillService.java
@@ -2,8 +2,13 @@ package com.lmbarquillo.curriculer.services;
 
 import com.lmbarquillo.curriculer.entities.User;
 import com.lmbarquillo.curriculer.models.DigitalSkillModel;
+import com.lmbarquillo.curriculer.models.SkillModel;
+
+import java.util.List;
 
 
-public interface DigitalSkillService {
+public interface SkillService {
     DigitalSkillModel getDigitalSkills(User user);
+
+    List<SkillModel> getOtherSkills(User user);
 }
