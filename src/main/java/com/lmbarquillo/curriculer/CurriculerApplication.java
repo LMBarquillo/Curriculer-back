bd85579640de3f07758ad04b39633e95d5757d3e
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-06-04 18:50:40 +0200

    Configuración para despliegue en Tomcat

--- src/main/java/com/lmbarquillo/curriculer/CurriculerApplication.java
+++ src/main/java/com/lmbarquillo/curriculer/CurriculerApplication.java
@@ -2,9 +2,10 @@ package com.lmbarquillo.curriculer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurriculerApplication {
+public class CurriculerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurriculerApplication.class, args);
