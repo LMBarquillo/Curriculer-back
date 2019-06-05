101fc54a97cea6986b622910eeab003335ce1aaa
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-06-05 19:23:49 +0200

    Configurado para despliegue en Docker

--- src/main/java/com/lmbarquillo/curriculer/CurriculerApplication.java
+++ src/main/java/com/lmbarquillo/curriculer/CurriculerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurriculerApplication extends SpringBootServletInitializer {
+public class CurriculerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurriculerApplication.class, args);
